d05dc321ec648d7f3b41965b8369faa7afbe123f
tstreamDOTh <tstream.h@outlook.com>
2020-05-30 22:47:01 +0530

    Update content

--- gatsby-config.js
+++ gatsby-config.js
@@ -22,10 +22,10 @@ module.exports = {
         name: `gatsby-starter-default`,
         short_name: `starter`,
         start_url: `/`,
-        background_color: `#663399`,
-        theme_color: `#663399`,
+        background_color: `#9990d7`,
+        theme_color: `#9990d7`,
         display: `minimal-ui`,
-        icon: `src/images/gatsby-icon.png`, // This path is relative to the root of the site.
+        icon: `src/images/favicon-16x16.png`, // This path is relative to the root of the site.
       },
     },
     // this (optional) plugin enables Progressive Web App + Offline functionality
--- src/pages/index.js
+++ src/pages/index.js
@@ -21,14 +21,23 @@ const IndexPage = () => (
     <section className="plain-info">
       <article>
         <h3>The Couple</h3>
-        <p>We are blessed to be surrounded by lovely people. </p>
+        <p>
+          We are together on an wonderful journey called - Life. <br></br>
+          <br></br>
+          Its been about 8 years together and we have decided to move forward in
+          live as husband & wife.
+        </p>
       </article>
       <img src={coupleImage} />
     </section>
     <section className="plain-info">
       <article>
         <h3>Our Lovely Family</h3>
-        <p>We are blessed to be surrounded by lovely people. </p>
+        <p>
+          We are blessed & fortunate to be surrounded by a lovely family.{" "}
+          <br></br>
+          <br></br>Without their support & belief, all this would be impossible.
+        </p>
       </article>
       <img src={familyImage} />
     </section>
